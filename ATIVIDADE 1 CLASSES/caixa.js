d915717c96a69e7fd071baa645c25e5b8434ac91
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-02-08 07:43:12 -0300

    Add files via upload

---
+++ ATIVIDADE 1 CLASSES/caixa.js
@@ -0,0 +1,13 @@
+this.x = 50;
+this.y = 50;
+this.largura = 100;
+this.altura = 50;
+
+
+show() {
+    rect(this.x, this.y, this.largura, this.altura);
+}
+
+definir_largura(l) {
+    this.w = l;
+}
---
+++ ATIVIDADE 1 CLASSES/sketch.js
@@ -0,0 +1,13 @@
+var caixa1, caixa2, caixa3;
+
+function setup() {
+    createCanvas(400, 400);
+    caixa1 = new Caixa();
+
+}
+
+function draw() {
+    background(220);
+    caixa1.show();
+
+}
